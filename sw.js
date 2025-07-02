7f74b19b08519836397b3765bded1b3f67b72853
@maden <166928262+maden77@users.noreply.github.com>
2025-07-02 20:24:33 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,29 +1,31 @@
-const CACHE_NAME = '2048-cache-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './classic.html',
-  './tertis.html',
-  './manifest.json',
-  './sw.js',
-  './icon2048-72x72.png',
-  './icon2048-96x96.png',
-  './icon2048-128x128.png',
-  './icon2048-144x144.png',
-  './icon2048-152x152.png',
-  './icon2048-192x192.png',
-  './icon2048-384x384.png',
-  './icon2048-512x512.png'
+const CACHE_NAME = '2048-pwa-v2'; // ganti versi untuk update
+const URLS_TO_CACHE = [
+  '/index.html',
+  '/classic.html',
+  '/tetris.html',
+  '/manifest.json',
+  '/sw.js',
+  '/icons/icon-72x72.png',
+  '/icons/icon-96x96.png',
+  '/icons/icon-128x128.png',
+  '/icons/icon-144x144.png',
+  '/icons/icon-152x152.png',
+  '/icons/icon-192x192.png',
+  '/icons/icon-384x384.png',
+  '/icons/icon-512x512.png'
+  // tambahkan JS/CSS lain jika perlu
 ];
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
   );
 });
 
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response =>
+      response || fetch(event.request)
+    )
   );
 });
